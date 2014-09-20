b29d2589694deaeecb1527f62942edea90dc12e8
Zachary Siegel <zack13532@gmail.com>
2014-09-20 15:31:45 -0400

    Imlpemented host and game loops.

--- src/com/doggydigits/seek/GameRound.java
+++ src/com/doggydigits/seek/GameRound.java
@@ -15,6 +15,55 @@ import android.view.MenuItem;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
+public class Player
+{
+    public int pnum;
+    public int team;
+    public int charge;
+    public int HP;
+    public double lat;
+    public double lon;
+    public double alt;
+
+    public Player(int player, int teamnum, int c, double la, double lo, double al)
+    {
+        pnum = player;
+        team = teamnum;
+        charge = c;
+        lat = la;
+        lon = lo;
+        alt = al;
+
+        HP = 0;
+    }
+
+    public setStatus(int c, int h, Location loc)
+    {
+        charge = c;
+        hp = h;
+        lat = loc.getLatitude();
+        lon = loc.getLongitude();
+        alt = loc.getAltitude();
+    }
+
+    // Turn a string into a usable object
+    public setStatus(String serializedBlast)
+    {
+        String[] res = serializedBlast.split(";;");
+        charge = Double.parseInt(res[0]);
+        lat = Double.parseDouble(res[1]);
+        lon = Double.parseDouble(res[2]);
+        alt = Double.parseDouble(res[3]);
+    }
+
+    // Serialize object
+    public String serializeStatus()
+    {
+        String toRet = String.valueOf(charge) + ";;" + hp + ";;" + lat + ";;" + lon + ";;" + alt;
+
+        return toRet;
+    }
+}
 
 /* Previous Activity MUST SEND PLAYER NUM AND GAME NAME AS "playerNum" and "gameName"
  * 
@@ -27,9 +76,16 @@ public class GameRound extends ActionBarActivity {
 	// charge represents this player's charge value: if -1, then the charge is not held. Otherwise, 0-3.
 	public String gameName;
 	public int HP, charge, playerNum;
-	
+
+    public int timeOffset;
+    public long startTime;
+    public long lastUpdate;
+    public int gameTime;
+    public int lastStunTime;
+
 	public int teamNum;
-	
+	public Player[] players;
+
 	// This is the intent that called this activity. (Usually the lobby.)
 	public Intent intent;
 	
@@ -38,14 +94,17 @@ public class GameRound extends ActionBarActivity {
 	
 	// TODO: Set max size of one game.
 	
-	public static int MAX_SIZE = 20;
+	public static final int MAX_SIZE = 20;
+    public static final double BLAST_RADIUS = 20;
+    public static final double SAFETY_RADIUS = 30;
 	
 	// This is only stored locally! 
 	// Records playerNums that may deal damage.
-	public HashSet<Integer> attackingMe;
+	//public HashSet<Integer> attackingMe;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+    {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_round);
 
@@ -67,9 +126,16 @@ public class GameRound extends ActionBarActivity {
 		// He also initializes the "charges" array.
 		// Otherwise, the player awaits the countdown to begin.
 		ParseObject p = new ParseObject(gameName);
-		
-		if (playerNum == 0){
-			p.put("started", 0);
+
+		players = new Player[p.getInt("numPlayers")];
+        for (int i = 0 ; i < players.length; i++)
+        {
+            players[i] = new Player(i, i % 2, 0, 0, 0, 0);
+        }
+
+		if (playerNum == 0)
+        {
+            /*
 			int[] array = new int[MAX_SIZE];
 			int i = 0;
 			while (i < MAX_SIZE){
@@ -79,20 +145,26 @@ public class GameRound extends ActionBarActivity {
 			
 			//p.put("charge", array);
 			//p.saveInBackground();
+			*/
+            p.put("started", 0);
 			beginCountdown();
-		} else // Players may begin in the countdown.
-			if (p.getInt("started") == 0) {
-				awaitCountdown();
 		}
-			else { // Game has already started...
-				//TODO: What do we do with people just entering midway?
-				//
-			}
+        else // Players may begin in the countdown.
+        {
+            if (p.getInt("started") == 0)
+                awaitCountdown();
+            else { // Game has already started...
+                //TODO: What do we do with people just entering midway?
+                //
+            }
+        }
+
 	}
 	
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+    {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.game_round, menu);
@@ -100,7 +172,8 @@ public class GameRound extends ActionBarActivity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item)
+    {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
@@ -114,7 +187,8 @@ public class GameRound extends ActionBarActivity {
 	/* Once the game starts, the host sends countdown data to the server.
 	 * 
 	 */
-	public void beginCountdown(){
+	public void beginCountdown()
+    {
 		ParseObject p = new ParseObject(gameName);
 		int timer = 30;
 		while (timer > 0){
@@ -132,13 +206,13 @@ public class GameRound extends ActionBarActivity {
 		// The timer is up, time to play!!
 		beginGame();
 	}
-	
-	public void awaitCountdown(){
+
+	public void awaitCountdown()
+    {
 		ParseObject p = new ParseObject(gameName);
-		int timer = p.getInt("timer");
+		int timer = 0;//p.getInt("timer");
 		while (timer > 0){
 			displayTimer();
-			timer = p.getInt("timer");
 			
 			// This is so the server isn't spammed with requests for the timer
 			try {
@@ -146,7 +220,7 @@ public class GameRound extends ActionBarActivity {
 			} catch (InterruptedException e) {
 				
 			}
-			
+            timer = p.getInt("timer");
 		}
 		
 		// The timer is up, time to play!!
@@ -154,7 +228,8 @@ public class GameRound extends ActionBarActivity {
 	}
 	
 	// Display timer animation.
-	public void displayTimer(){
+	public void displayTimer()
+    {
 		//TODO: display timer animation
 	}
 	
@@ -165,7 +240,10 @@ public class GameRound extends ActionBarActivity {
 
 		LocationListener locationListener = new LocationListener() {
 		    public void onLocationChanged(Location location) {
-		    	loc = location;
+                double accsqrt = Math.sqrt(location.getAccuracy());
+		    	loc.setLatitude((1 - accsqrt) * loc.getLatitude() + accsqrt * location.getLatitude());
+                loc.setLongitude((1 - accsqrt) * loc.getLongitude() + accsqrt * location.getLongitude());
+                loc.setAltitude((1 - accsqrt) * loc.getAltitude() + accsqrt * location.getAltitude());
 		    }
 
 		    public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -175,17 +253,104 @@ public class GameRound extends ActionBarActivity {
 		    public void onProviderDisabled(String provider) {}
 		  };
 		
-		// Register the listener with the Location Manager to receive location updates once per second
-			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-	
-		// Next, we retrieve current data from the server.
-			retrieveData();
+		// Register the listener with the Location Manager to receive location updates twice per second
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 250, 0, locationListener);
+
+        // Host loop
+        if (playerNum == 0)
+        {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // Games are 6 minutes long
+                    while (gameTime < 3600) {
+                        try
+                        {
+                            Thread.sleep(500);
+                            mHandler.post(new Runnable(){
+
+                                @Override
+                                public void run()
+                                {
+
+                                    gameTime = (int)((System.currentTimeMillis() - startTime - 30000) * 100);
+
+                                    ParseObject p = new ParseObject(gameName);
+                                    p.put("time", gameTime);
+                                }
+                            });
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        // Game loop thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (gameTime < 3600) {
+                    try {
+                        Thread.sleep(500);
+                        mHandler.post(new Runnable() {
+
+                            @Override
+                            public void run()
+                            {
+
+                                ParseObject p = new ParseObject(gameName);
+                                gameTime = p.getInt("time");
+                                lastUpdate = System.currentTimeMillis();
+
+                                // Get locations and status of each player
+                                for(int i = 0; i < players.length; i++)
+                                    players[i].setStatus(p.getString(i + "::status"));
+
+                                // Check for charge detonations
+                                for(int i = 0; i < players.length; i++)
+                                {
+                                    // If another player is within a reasonable range
+                                    // and will be detonating soon, spawn a new thread to handle
+                                    // damage detection
+                                    double dist = getDist(players[i]);
+                                    final int countdown = gameTime - players[i].charge;
+                                    if(dist < SAFETY_RADIUS && countdown < 8 && countdown > 0))
+                                    {
+                                        new Thread(new Runnable() {
+                                            public void run() {
+                                                Thread.sleep(countdown * 95);
+                                                ParseObject newP = new ParseObject(gameName);
+                                                players[i].setStatus(p.getString(i + "::status"));
+
+                                                if(dist(players[i]) < BLAST_RADIUS)
+                                                    damageCalculation(players[i], dist, gameTime + countdown);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                // Set own location
+                                p.put(playerNum + "::", serializeStatus());
+                            }
+                        });
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                }
+            }
+        }).start();
+
 	}
 	
 // The LOCAL integer charge represents the current player's charge value.
 // The SERVER'S charge is an array of ALL charges by all players.
-	
-public void sendData(int[] chargeOld){
+
+    /*
+    public void sendData(int[] blasts)
+    {
 		ParseObject p = new ParseObject(gameName);
 		p.put(playerNum + "HP", HP);
 		p.put(playerNum + "lat", String.valueOf(loc.getLatitude()));
@@ -196,33 +361,39 @@ public void sendData(int[] chargeOld){
 		p.put("charge", chargeOld);
 		p.saveInBackground();
 	}
-	
-	public void retrieveData(){
+    */
+
+    /*
+	public void retrieveData()
+    {
 		
 		ParseObject p = new ParseObject(gameName);
 		int[] chargeOld = (int[]) p.get("charge");
 		
 		// If something fucks up, then there's likely a key missing so we send it anyway
-		try {
-		String locOldLat = (String) p.get(playerNum + "lat");
-		String locOldLong = (String) p.get(playerNum + "long");
-		String locOldAlt = (String) p.get(playerNum + "alt");
-		int HPOld = p.getInt(playerNum + "HP");
-		
-		Location locOld = new Location(loc);
-		locOld.setLatitude(Double.valueOf(locOldLat));
-		locOld.setLongitude(Double.valueOf(locOldLong));
-		locOld.setAltitude(Double.valueOf(locOldAlt));
-		
-		// If the player's status has been updated, reflect it in the server.
-		// Otherwise, we don't care about updating.
-		if (!loc.equals(locOld) || HPOld != HP || charge != chargeOld[playerNum]){
-			sendData(chargeOld);
-		}
-		
-		
-		}
-		catch (Exception e){
+		try
+        {
+            String locOldLat = (String) p.get(playerNum + "lat");
+            String locOldLong = (String) p.get(playerNum + "long");
+            String locOldAlt = (String) p.get(playerNum + "alt");
+            int HPOld = p.getInt(playerNum + "HP");
+
+            Location locOld = new Location(loc);
+            locOld.setLatitude(Double.valueOf(locOldLat));
+            locOld.setLongitude(Double.valueOf(locOldLong));
+            locOld.setAltitude(Double.valueOf(locOldAlt));
+
+            // If the player's status has been updated, reflect it in the server.
+            // Otherwise, we don't care about updating.
+            if (!loc.equals(locOld) || HPOld != HP || charge != chargeOld[playerNum])
+            {
+                sendData(chargeOld);
+            }
+
+
+        }
+		catch (Exception e)
+        {
 			sendData(chargeOld);
 		}
 		
@@ -231,10 +402,41 @@ public void sendData(int[] chargeOld){
 		
 		
 		
-	}
-	
-	public void damageCalculation()
+	}*/
+
+    public void chargeButtonPressed(View view)
+    {
+        if(HP > 0 && gameTime - lastStunTime > 30)
+        {
+            lastStunTime = (int)((System.currentTimeMillis() - lastUpdate) * 100) + gameTime;
+            charge = lastStunTime + 35;
+        }
+    }
+
+    public double getDist(Player p)
+    {
+        Location l = new Location();
+        l.setLatitude(p.lat);
+        l.setLongitude(p.lon);
+        l.setAltitude(p.alt);
+
+        return loc.distanceTo(l);
+    }
+
+	public void damageCalculation(Player p, double dist, int gameTime)
 	{
+        int damage = 80 * (1 - (BLAST_RADIUS - dist) / BLAST_RADIUS);
+        HP = Math.max(HP - damage, 0);
+
+        lastStunTime = gameTime;
 	}
+
+    public String serializeStatus()
+    {
+        String toRet = String.valueOf(charge) + ";;" + HP + ";;" + loc.getLatitude() +
+                ";;" + loc.getLongitude() + ";;" + loc.getAltitude();
+
+        return toRet;
+    }
 
 }
